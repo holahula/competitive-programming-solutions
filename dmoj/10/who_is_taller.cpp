7f4c783c50ccb77b0249ecdff518a7ab1213ee3e
Austin Jiang <a27jiang@uwaterloo.ca>
2018-05-08 11:05:23 -0400

    e

--- dmoj/10/who_is_taller.cpp
+++ dmoj/10/who_is_taller.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 
 int main() {
+int class [1000005][2][10000005]
+int n, m, p, q, x, y;
 
+cin >> n >> m;
 
+for(int i = 0; i < m; i++){
+  cin >> x >> y; // x is taller than y
+  class[x]
+
+}
   return 0;
 }
