375e8b1661da9c9b01f3050827f5a814653d7801
Austin Jiang <austinjiang@Austins-MacBook-Pro.local>
2018-05-11 12:05:15 -0400

    e

--- dmoj/10/who_is_taller.cpp
+++ dmoj/10/who_is_taller.cpp
@@ -2,26 +2,40 @@
 
 using namespace std;
 
+bool bfs(vector<int>scale, int at, int q){
+  for(int i = 1; i <= scale[at].size(); i++){
+    if(scale[at][i] == q){
+      return true;
+    } else {
+      bfs(scale, scale[at][i], q);
+    }
+  }
+  return false;
+}
 
 int main() {
-int class [1000005][2][10000005];
-vector<vector<vector<int>int>int>>> class(1000005, vector<int>)
-for(int i = 0; i < 1000005; i++){
-  for(int j = 0; j < 2; j++){
-    for(int k = 0; k < 10000005; k++({
 
-    })
-  }
-}
+vector<vector<int>> taller (1000005); // x is taller than ...
+vector<vector<int>> shorter (1000005); // y is shorter than ...
 int n, m, p, q, x, y;
 
 cin >> n >> m;
 
 for(int i = 0; i < m; i++){
   cin >> x >> y; // x is taller than y
-  class[x]
-
+  shorter[x].push_back(y);
+  taller[y].push_back(x);
 }
+
+cin >> p >> q;
+
+if (bfs(taller, p, q)){
+  cout << "yes" << endl;
+} else if (bfs(shorter, p, q)){
+  cout << "no" << endl;
+} else {
+  cout << "unknown" << endl;
+}
+
   return 0;
 }
-vector[1][2].push_back();
