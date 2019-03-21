3c50427e37c63f665bde759ed969446816072242
jiang-austin <austin.jiang@uwaterloo.ca>
2019-03-20 22:06:28 -0700

    day 15 - reservoir sampling

---
+++ daily-coding-problem/15/15.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+func random(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
+
+func choose(stream []int) int {
+	element := 0
+
+	for i := 0; i < len(stream); i++ {
+		if random(1, i+1) == 1 {
+			element = stream[i]
+		}
+	}
+	return element
+}
+
+func main() {
+	stream := []int{}
+
+	choose(stream)
+}
