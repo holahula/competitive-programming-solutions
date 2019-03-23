aa2a37a51180d0d99ba6857d2e2d547950637874
jiang-austin <austin.jiang@uwaterloo.ca>
2019-03-22 21:39:23 -0700

    day 18 - O(N) solution

--- daily-coding-problem/18/18.go
+++ daily-coding-problem/18/18.go
@@ -30,6 +30,13 @@ type deque struct {
 	arr []int
 }
 
+func Deque() *deque {
+	dq := &deque{
+		len: 0,
+		arr: []int{},
+	}
+	return dq
+}
 func (dq *deque) popLeft() {
 	dq.arr = dq.arr[1:]
 	dq.len--
@@ -49,13 +56,40 @@ func (dq *deque) pushRight(a int) {
 }
 
 func max(arr []int, k int) {
+	dq := Deque()
+	dq.pushRight(0)
+	iter := 1
+	//pre-process
+	for dq.len < k {
+		if arr[iter] > arr[dq.arr[0]] {
+			dq.pushLeft(iter)
+		} else {
+			dq.pushRight(iter)
+		}
+		iter++
+	}
+	// fmt.Println(k-1, dq.arr)
 
+	for i := k; i < len(arr); i++ {
+		fmt.Println(arr[dq.arr[0]])
+
+		// pop left until dq[0] is inside current sub-array
+		for dq.len > 0 && dq.arr[0] <= i-k {
+			dq.popLeft()
+		}
+		// if inserting element is greater than dq[0], pop a
+		for dq.len > 0 && arr[i] > arr[dq.arr[0]] {
+			dq.popRight()
+		}
+		dq.pushRight(i)
+		// fmt.Println(i, dq.arr)
+	}
+	fmt.Println(arr[dq.arr[0]])
 }
 
 func bf(arr []int, k int) {
 	for i := 0; i < len(arr)-k+1; i++ {
 		sub := arr[i : i+k]
-		fmt.Println(sub)
 		max := sub[0]
 		for i := 0; i < k; i++ {
 			if sub[i] > max {
@@ -67,7 +101,7 @@ func bf(arr []int, k int) {
 }
 
 func main() {
-	arr := []int{10, 5, 2, 7, 8, 7}
+	arr := []int{10, 1, 1, 1, 1, 2, 1, 1, 1}
 	k := 3
 	bf(arr, k)
 	max(arr, k)
